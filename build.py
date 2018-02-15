2d36686de75127c616aa0098cc01878835f5b272
NARVIS-UECK\ueck <ulrich.eck@tum.de>
2018-02-15 16:09:30 +0100

    maybe fix travis build on linux skip x86 shared lib variants

--- build.py
+++ build.py
@@ -1,7 +1,14 @@
 from conan.packager import ConanMultiPackager
+from conans.tools import os_info
+import platform
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="flatbuffers:shared", pure_c=True)
+    builder.builds = [
+        [settings, options, env_vars, build_requires]
+        for settings, options, env_vars, build_requires in builder.builds
+        if (not (os_info.is_linux and settings["arch"] == "x86")) and options['flatbuffers:shared'] == True 
+    ]
     builder.run()
