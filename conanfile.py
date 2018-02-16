aefc33811b041b5f42305dc35d9b7f25a4b0c9c8
Ulrich Eck <ueck@net-labs.de>
2018-02-16 17:30:48 +0100

    fix typo

--- conanfile.py
+++ conanfile.py
@@ -79,4 +79,4 @@ class FlatBuffersConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.env_info.PATH.append(os.path.join(self.package_folder, "bin")
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
